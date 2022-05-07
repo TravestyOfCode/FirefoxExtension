0a62f5c7b8d13470244b83ca57d3e03762d59663
hpjchobbes <hpjchobbes@yahoo.com>
2022-05-06 23:07:55 -0400

    Adds beastify.js

---
+++ Beastify/content_scripts/beastify.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    /** Check/set global guard variable so it won't
+     * do anything if it gets injected a second time.
+     **/
+
+    if (window.hasRun) {
+        return;
+    }
+
+    window.hasRun = true;
+
+    /** Given an url to a beast image, remove all existing beasts
+     * then create and style an IMG node pointing to that image and
+     * insert the node into the document.
+     **/
+
+    function insertBeast(beastURL) {
+        removeExisitingBeasts();
+        let beastImage = document.createElement("img");
+        beastImage.setAttribute("src", beastURL);
+        beastImage.style.height = "100vh";
+        beastImage.className = "beastify-image";
+        document.body.appendChild(beastImage);
+    }
+
+    /** Remove every beast from the page **/
+    function removeExistingBeasts() {
+        let existingBeasts = document.querySelectorAll(".beastify-image");
+        for (let beast of existingBeasts) {
+            beast.remove();
+        }
+    }
+
+    /** Listen for message from the background script
+     * Call insertBeast() or removeExistingBeasts()
+     **/
+    browser.runtime.onMessage.addListener((message) => {
+        if (message.command === "beastify") {
+            insertBeast(message.beastURL);
+        }
+        else if (message.command === "reset") {
+            removeExistingBeasts();
+        }
+    });
+
+
+})();
